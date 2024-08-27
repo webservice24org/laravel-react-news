edca77ca36a53cc5a5ed84cdf490a166bbb51c7f
webservice24org <webservice24.org@gmail.com>
2024-08-27 13:36:04 +0600

    FrontEnd Updated

--- newsreact/src/components/frontend/Openion.jsx
+++ newsreact/src/components/frontend/Openion.jsx
@@ -1,35 +1,72 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import axiosInstance from '/axiosConfig';
+import { Link } from 'react-router-dom';
 
 
 const Openion = () => {
+  const [posts, setPosts] = useState([]);
+  const baseURL = axiosInstance.defaults.baseURL;
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axiosInstance.get('api/posts-by-category?category=মতামত');
+        setPosts(response.data.data);
+      } catch (error) {
+        setPosts([]);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  const lastPost = posts.length > 0 ? posts[0] : null;
+  const nextPosts = posts.length > 1 ? posts.slice(1, 4) : [];
+
+
   return (
     <div class="category_card_news_item">
         <div class="card">
             <div class="card-header">
-            <a href="#"><h2 class="card-title">মতামত</h2></a>
+                {posts.length > 0 && (
+                    <Link to={`/category/${posts[0].category.category_id}/posts`}>
+                        <h2>মতামত</h2>
+                    </Link>
+                )}
             </div>
             <div class="card-body">
-                <div class="top_news">
-                    <div class="img_box">
-                        <a href="single.html"><img class="img-fluid" src="/src/assets/frontend/img/3003043823_n.webp" alt="news title" /></a>
+                {lastPost && (
+                    <div class="top_news">
+                        <div class="img_box">
+                            <a href={`/post/${lastPost.id}`}>
+                                <img 
+                                    className="img-fluid" 
+                                    src={`${baseURL}storage/post/${lastPost.post_thumbnail}`} 
+                                    alt={lastPost.post_title} 
+                                />
+                            </a>
+                        </div>
+                        <a href={`/post/${lastPost.id}`}>
+                            <h2 class="mt-1">{lastPost.post_title}</h2>
+                        </a>
                     </div>
-                    <a href="single.html"><h2>সন্তানের জন্মদিনে মদ ও ধূমপান ছেড়েছেন রণবীর
-
-                    </h2></a>
-                </div>
+                )}
                 <div class="list_news">
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item"><a href="single.html"><h2>ঘূর্ণিঝড়ে ক্ষতিগ্রস্ত রোহিঙ্গাদের জরুরি ত্রাণসহায়তা প্রয়োজন: জাতিসংঘ</h2></a></li>
-                        <li class="list-group-item"><a href="single.html"><h2>ঘূর্ণিঝড়ে ক্ষতিগ্রস্ত রোহিঙ্গাদের জরুরি ত্রাণসহায়তা প্রয়োজন: জাতিসংঘ</h2></a></li>
-                        <li class="list-group-item"><a href="single.html"><h2>ঘূর্ণিঝড়ে ক্ষতিগ্রস্ত রোহিঙ্গাদের জরুরি ত্রাণসহায়তা প্রয়োজন: জাতিসংঘ</h2></a></li>
-                        <li class="list-group-item"><a href="single.html"><h2>ঘূর্ণিঝড়ে ক্ষতিগ্রস্ত রোহিঙ্গাদের জরুরি ত্রাণসহায়তা প্রয়োজন: জাতিসংঘ</h2></a></li>
-                        
+                        {nextPosts.map((post, index) => (
+                            <li class="list-group-item" key={index}>
+                                <a href={`/post/${post.id}`}><h2>{post.post_title}</h2></a>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
             <div class="card-footer">
-                <a href="category.html"><h2>আরও দেখুন</h2></a>
+            {posts.length > 0 && (
+                    <Link to={`/category/${posts[0].category.category_id}/posts`}>
+                        <h2>আরও দেখুন</h2>
+                    </Link>
+                )}
             </div>
         </div>
     </div>
